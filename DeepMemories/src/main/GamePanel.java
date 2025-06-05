7e630016a58907f5d6ae7740ed4a95787fd68cb5
Caio Riciati <esdrascaio5313@gmail.com>
2025-06-05 18:38:16 -0300

    bug do boolean
    arrumei o boolean, eu tinha inicializado ele com letra maiuscula (Boolean) e com ele recebendo valor null inicialmente n estava deixando o update criar o loop

--- DeepMemories/src/main/GamePanel.java
+++ DeepMemories/src/main/GamePanel.java
@@ -15,17 +15,25 @@ public class GamePanel extends JPanel implements Runnable{
 
 	final int tileSize = originalTileSize * scale;
 
-	final int maxScreenCol = 18;
-	final int maxScreenRow = 12;
+	final int maxScreenCol = 25;
+	final int maxScreenRow = 15;
 	final int screenWidth = tileSize * maxScreenCol; //769pixels
 	final int screenHeight = tileSize * maxScreenRow;//576pixels
 	
+	KeyHandler KeyH = new KeyHandler();
 	Thread gameThread;
-
+	
+	//set Player's default position
+	int playerX= 100;
+	int playerY = 100;
+	int playerSpeed = 4;
+	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.LIGHT_GRAY);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(KeyH);
+		this.setFocusable(true);
 		
 	}
 	
@@ -38,7 +46,7 @@ public class GamePanel extends JPanel implements Runnable{
 	@Override
 	public void run() {
 		while (gameThread != null) {
-		//	System.out.println("The game loop is running");
+			System.out.println("The game loop is running");
 			//Update: information such as character positions
 			update();
 			//Draw: draw the screen with the updated information
@@ -47,15 +55,30 @@ public class GamePanel extends JPanel implements Runnable{
 		
 	}
 	public void update() {
-		
+		if(KeyH.upPressed == true) {
+			playerY -= playerSpeed;
+		}
+		else if(KeyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		else if(KeyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		else if(KeyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
 	}
-	public void paintComponet(Graphics g) {
+	
+	@Override
+	public void paintComponent(Graphics g) {
 	
 		super.paintComponent(g);
 		Graphics2D g2= (Graphics2D)g;
 		
 		g2.setColor(Color.white);
-		g2.fillRect(100, 100, tileSize, tileSize);
+		
+		g2.fillRect(playerX, playerY, tileSize, tileSize);
+		
 		g2.dispose();
 	}
 	
