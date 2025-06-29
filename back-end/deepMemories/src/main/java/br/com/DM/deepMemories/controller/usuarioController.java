d2215a1b3f5d517d6392afa1cd687642011a3cf1
Caio Riciati <esdrascaio5313@gmail.com>
2025-06-29 00:14:15 -0300

    requisições HTTP

--- back-end/deepMemories/src/main/java/br/com/DM/deepMemories/controller/usuarioController.java
+++ back-end/deepMemories/src/main/java/br/com/DM/deepMemories/controller/usuarioController.java
@@ -3,7 +3,12 @@ package br.com.DM.deepMemories.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.DM.deepMemories.DAO.IUsuario;
@@ -11,13 +16,36 @@ import br.com.DM.deepMemories.entity.Usuario;
 
 
 @RestController
+@RequestMapping("usuarios")
 public class usuarioController {
 	
 	@Autowired
 	private IUsuario dao;
 
-		@GetMapping("usuarios")
+		@GetMapping
 		public List<Usuario> listaUsuarios() {
 			return (List<Usuario>) dao.findAll();
 		}
+		
+		@PostMapping
+		public Usuario criarUsuario(@RequestBody Usuario usuario) {
+			Usuario usuarioNovo = dao.save(usuario);
+			return usuarioNovo;
+		}
+		
+		@PutMapping
+		public Usuario editarUsuario(@RequestBody Usuario usuario) {
+			Usuario usuarioNovo = dao.save(usuario);
+			return usuarioNovo;
+		}
+		
+		
+		
+		@DeleteMapping
+		public Usuario removerUsuario(Usuario usuario) {
+			Usuario usuarioRemovido = dao.delete(usuario);
+			return usuarioRemovido;
+		}
+		
+
 }
