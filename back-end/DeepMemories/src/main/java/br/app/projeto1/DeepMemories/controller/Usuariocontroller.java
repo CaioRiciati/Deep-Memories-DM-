b3094c0f363f0135ce370fd6372e5b149228d1b7
Caio Riciati <esdrascaio5313@gmail.com>
2025-08-25 23:42:50 -0300

    criei a API, mas ainda n tem endpoints, vou add amanhã

--- back-end/DeepMemories/src/main/java/br/app/projeto1/DeepMemories/controller/Usuariocontroller.java
+++ back-end/DeepMemories/src/main/java/br/app/projeto1/DeepMemories/controller/Usuariocontroller.java
@@ -1,9 +1,19 @@
 package br.app.projeto1.DeepMemories.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Usuariocontroller {
 	@GetMapping("/usuario")
 	public String texto () {
 		return "acessando a api";
+		
+	}
+	
+	@GetMapping("/carro")
+	public String texto2() {
+		return "katchaun";
+		
 	}
 }
